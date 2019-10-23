fa5aac08d3d74ee6fe32f5fcfd78ba6d02c090b2
Luis Mantilla <lcmantilla@gmail.com>
2019-10-23 09:06:41 -0400

    Fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -11,18 +11,32 @@ export class InfoPaginaService {
   info: infoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-    console.log("Servico Pagina listo...");
-
+  private cargarInfo() {
     // Leer el archivo json
     this.http.get('assets/data/data-pagina.json')
     .subscribe( ( resp: infoPagina ) => {
       this.cargada = true;
       this.info = resp;
+      //console.log( resp );
+
+    });
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-b0744.firebaseio.com/equipo.json')
+    .subscribe( ( resp: any ) => {
+      this.cargada = true;
+      this.equipo = resp;
       console.log( resp );
 
-    })
-
+    });
   }
+
 }
